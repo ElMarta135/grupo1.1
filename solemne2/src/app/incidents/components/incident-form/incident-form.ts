4e1a74a11cc49da400c2811e765e7c43841673e6
Marto <martiin.rtl@gmail.com>
2025-06-08 17:47:20 -0400

    incident-form

---
+++ solemne2/src/app/incidents/components/incident-form/incident-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-form',
+  imports: [],
+  templateUrl: './incident-form.html',
+  styleUrl: './incident-form.css'
+})
+export class IncidentForm {
+
+}
