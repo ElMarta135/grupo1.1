eb7b83d76d73c2a5ec3ad9194faa742d4731cfe8
Marto <martiin.rtl@gmail.com>
2025-06-08 17:50:46 -0400

    loading routes

--- solemne2/src/app/incidents/incidents-module.ts
+++ solemne2/src/app/incidents/incidents-module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IncidentsRoutingModule } from './incidents-routing-module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    IncidentsRoutingModule
   ]
 })
 export class IncidentsModule { }
